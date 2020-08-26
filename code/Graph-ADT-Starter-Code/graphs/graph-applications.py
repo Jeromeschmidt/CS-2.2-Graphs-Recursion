711e210de23da406b037f7647551bd3e402d6fd8
Jerome <jerome.schmidt@students.makeschool.com>
2020-08-25 20:13:34 -0500

    finished courseOrder

--- code/Graph-ADT-Starter-Code/graphs/graph-applications.py
+++ code/Graph-ADT-Starter-Code/graphs/graph-applications.py
@@ -1,4 +1,4 @@
-# from graphs.graph import Graph, Vertex
+from graphs.graph import Graph, Vertex
 from collections import deque
 
 def dfs(grid, start_pos, visited):
@@ -93,7 +93,6 @@ def timeToRot(grid):
                 num_of_oranges -= 1
                 grid[temp[0]][temp[1]] = 2
                 changes += 1
-                print(changes)
 
         if changes == 0:
             return -1
@@ -123,19 +122,27 @@ oranges3 = [
 assert timeToRot(oranges3) == 0
 
 
-# def courseOrder(numCourses, prerequisites):
-#     """Return a course schedule according to the prerequisites provided."""
-#     pass
-#
-# # Test Cases
-# courses1 = [ [1,0] ]
-# assert courseOrder(2, courses1) == [0, 1]
-#
-# courses2 = [ [1,0], [2,0], [3,1], [3,2] ]
-# possibleSchedules = [ [0, 1, 2, 3], [0, 2, 1, 3] ]
-# assert courseOrder(4, courses2) in possibleSchedules
-#
-#
+def courseOrder(numCourses, prerequisites):
+    """Return a course schedule according to the prerequisites provided."""
+    graph = Graph(is_directed=True)
+
+    for i in range(numCourses):
+        graph.add_vertex(i)
+
+    for elm in prerequisites:
+        graph.add_edge(elm[1], elm[0])
+
+    return graph.topological_sort()
+
+# Test Cases
+courses1 = [ [1,0] ]
+assert courseOrder(2, courses1) == [0, 1]
+
+courses2 = [ [1,0], [2,0], [3,1], [3,2] ]
+possibleSchedules = [ [0, 1, 2, 3], [0, 2, 1, 3] ]
+assert courseOrder(4, courses2) in possibleSchedules
+
+
 # def wordLadderLength(beginWord, endWord, wordList):
 #     """Return the length of the shortest word chain from beginWord to endWord, using words from wordList."""
 #     pass
@@ -145,4 +152,4 @@ assert timeToRot(oranges3) == 0
 # endWord = "cog"
 # wordList = ["hot","dot","dog","lot","log","cog"]
 #
-# assert wordLadderLength(beginWord, endWord, wordList) ==
+# assert wordLadderLength(beginWord, endWord, wordList) == 5
